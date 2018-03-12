29dc93b6d94904892375a240afa3a4a8448c405f
Mack <amack@proforma.com>
2018-03-12 15:20:29 -0400

    html layout

--- public/main.js
+++ public/main.js
@@ -1,5 +1,15 @@
+let team1Name = "Team 1";
+let team1Score = 0;
+
+let team2Name = "Team2";
+let team2Score = 0;
+
 const main = () => {
-  document.querySelector('h1').textContent += '?'
+   // select the team 1 add button
+   // give it a click handler (a function)
+   // in that handler,
+   //    increment the score for that team
+   //    find the element with score and update it to the new score
 }
 
 document.addEventListener('DOMContentLoaded', main)
