69c6b60b3d8a1820bb0fbef823c1c7c396632651
Mack <amack@proforma.com>
2018-03-13 12:27:19 -0400

    refractored code

--- public/main.js
+++ public/main.js
@@ -22,20 +22,30 @@ const main = () => {
   const team2Name = "Team2";
   let team2Score = 0;
 
-  const mybutton = document.querySelector(".button1");
+  const mybutton1 = document.querySelector(".button1");
+  const mybutton2 = document.querySelector(".button2");
+  const mybutton3 = document.querySelector(".button3");
+  const mybutton4 = document.querySelector(".button4");
   const myscore1 = document.querySelector(".team1-result .team1");
   const myscore2 = document.querySelector(".team2-result .team2");
+
 
   mybutton.textContent = "Increase by one";
   console.log(mybutton);
 
-  mybutton.addEventListener("click", (event) => {
+  mybutton1.addEventListener("click", (event) => {
       team1Score += 1;
+      myscore1.textContent ="add one"
       console.log(team1Score);
+  })
+   mybutton2.addEventListener("click", (event) => {
+        team1Score -= 1;
+        myscore1.textContent ="subtract one"
+        console.log(team1Score);
   });
   // in that handler,
   //    increment the score for that team
   //    find the element with score and update it to the new score
 }
 
-document.addEventListener('DOMContentLoaded', main)
+document.addEventListener('DOMContentLoaded', main);
