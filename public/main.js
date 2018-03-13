5d5148e2d521a351112c4b593f52c2c358b0227b
Mack <amack@proforma.com>
2018-03-13 07:58:09 -0400

    added query to team1 & team2

--- public/main.js
+++ public/main.js
@@ -23,8 +23,16 @@ const main = () => {
   let team2Score = 0;
 
   const mybutton = document.querySelector(".button1");
+  const myscore1 = document.querySelector(".team1-result .team1");
+  const myscore2 = document.querySelector(".team2-result .team2");
 
   mybutton.textContent = "Increase by one";
+  console.log(mybutton);
+
+  mybutton.addEventListener("click", (event) => {
+      team1Score += 1;
+      console.log(team1Score);
+  });
   // in that handler,
   //    increment the score for that team
   //    find the element with score and update it to the new score
